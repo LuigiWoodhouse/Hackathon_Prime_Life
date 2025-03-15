3941b7036f77585c08a64c644ee30f4971bac4db
Luigi Woodhouse <luigiwoodhouse@gmail.com>
2025-03-15 16:00:11 -0500

    Add api, swagger config,security

--- Backend/src/main/java/com/primelife/entity/Appointments.java
+++ Backend/src/main/java/com/primelife/entity/Appointments.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -13,7 +14,7 @@ import java.util.Date;
 
 @Data
 @Entity
-
+@Table(name ="appointments")
 public class Appointments {
 
     @Id
